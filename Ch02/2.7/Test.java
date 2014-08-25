01cb8cbeca11e1c17ea31eab1f11e879da3a754b
darrenhuang22 <darrenhuang22@gmail.com>
2014-08-25 18:10:15 -0400

    Yinjie Huang

---
+++ Ch02/2.7/Test.java
@@ -0,0 +1,54 @@
+// 2.7 Implement a function to check if a linked list a palindrome
+// I have the right idea of using the stack, very good
+//
+import java.util.*;
+
+class Test{
+        public static void main(String[] args){
+                System.out.println("CareerCup 2.7!!!");
+                int[] a = new int[]{1,2,0,2,1};
+                ListNode list = new ListNode(1);
+                ListNode current = list;
+                for (int i = 1; i < a.length; i++){
+                        current.next = new ListNode(a[i]);
+                        current = current.next;
+                }
+                Palindrome p = new Palindrome();
+                System.out.println(p.isPalindrome(list));
+        }
+}
+
+class ListNode{
+    int key;
+    ListNode next;
+
+    ListNode(int key){
+            this.key = key;
+            this.next = null;
+    }
+}
+
+class Palindrome{
+    public boolean isPalindrome(ListNode head){
+            ListNode slow = head;
+            ListNode fast = head;
+            Stack<Integer> s = new Stack<Integer>();
+
+            while (fast != null && fast.next != null){
+                    s.push(slow.key);
+                    slow = slow.next;
+                    fast = fast.next.next;
+            }
+
+            if (fast != null)
+                    slow = slow.next;
+
+            while (!s.empty()){
+                    if (s.pop() == slow.key)
+                            slow = slow.next;
+                    else
+                            return false;
+            }
+            return true;
+    }
+}
